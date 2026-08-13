9c37e04df0dbb096254fe860ba2383816af85ce1
usman <muhammadusman67200@gmail.com>
2026-08-13 14:48:52 +0500

    add employee app

--- conf/settings.py
+++ conf/settings.py
@@ -46,9 +46,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "rest_framework_simplejwt",
+    "corsheaders",
     "rest_framework",
     "accounts",
+    "employees",
 ]
 
 MIDDLEWARE = [
