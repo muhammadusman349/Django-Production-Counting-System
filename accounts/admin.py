3a41df6fbf230f25c5210a8110d0a02234fa6985
usman <muhammadusman67200@gmail.com>
2026-08-13 11:46:47 +0500

    update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,36 @@ from django.contrib import admin
 from .models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_active",
+        "is_staff",
+        "created_at",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+        "phone",
+    )
+
+    ordering = ("-created_at",)
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
