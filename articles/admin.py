f78d1c5c6b974379bc58e5c95dbca5867f31b05e
usman <muhammadusman67200@gmail.com>
2026-08-15 16:47:27 +0500

    update admin .py

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, ArticleSize, ArticleImage
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "article_code",
+        "name",
+        "is_active",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
+    search_fields = (
+        "article_code",
+        "name",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+
+@admin.register(ArticleSize)
+class ArticleSizeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "article",
+        "size",
+        "created_at",
+    )
+
+    list_filter = (
+        "size",
+    )
+
+    search_fields = (
+        "article__article_code",
+        "article__name",
+        "size",
+    )
+
+
+@admin.register(ArticleImage)
+class ArticleImageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "article",
+        "created_at",
+    )
+
+    search_fields = (
+        "article__article_code",
+        "article__name",
+    )
