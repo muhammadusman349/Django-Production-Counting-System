b4199b80a5458b8f29d53bc13c5b34e3756dcdc2
usman <muhammadusman67200@gmail.com>
2026-08-15 13:03:35 +0500

    UPDATED APIS & URLS

--- employees/urls.py
+++ employees/urls.py
@@ -1,31 +1,17 @@
 from django.urls import path
 
-from .views import DepartmentApiView, EmployeeApiView
+from .views import (
+    DepartmentApiView,
+    EmployeeApiView,
+)
 
 
 urlpatterns = [
-    path(
-        "departments/",
-        DepartmentApiView.as_view(),
-        name="departments"
-    ),
+    # Department
+    path("departments/",DepartmentApiView.as_view(),name="department-list-create"),
+    path("departments/<int:id>/",DepartmentApiView.as_view(),name="department-detail"),
 
-    path(
-        "departments/<int:id>/",
-        DepartmentApiView.as_view(),
-        name="department-detail"
-    ),
-
-    path(
-        "employees/",
-        EmployeeApiView.as_view(),
-        name="employees"
-    ),
-
-    path(
-        "employees/<int:id>/",
-        EmployeeApiView.as_view(),
-        name="employee-detail"
-    ),
+    # Employee
+    path("employees/",EmployeeApiView.as_view(),name="employee-list-create"),
+    path("employees/<int:id>/",EmployeeApiView.as_view(),name="employee-detail"),
 ]
-
--- employees/views.py
+++ employees/views.py
@@ -1,73 +1,30 @@
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 
 from .models import Department, Employee
 from .serializers import DepartmentSerializer, EmployeeSerializer
 
 
-class DepartmentApiView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+class DepartmentApiView(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = DepartmentSerializer
     queryset = Department.objects.all()
+    lookup_field = "id"
 
     def get(self, request, *args, **kwargs):
-        if "id" in kwargs:
-            department = self.get_object()
-            serializer = self.get_serializer(department)
+        if "id" in self.kwargs:
+            return self.retrieve(request, *args, **kwargs)
 
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK
-            )
-
-        departments = self.get_queryset()
-        serializer = self.get_serializer(
-            departments,
-            many=True
-        )
-
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
-
-    def get_object(self):
-        return generics.get_object_or_404(
-            self.get_queryset(),
-            id=self.kwargs["id"]
-        )
+        return self.list(request, *args, **kwargs)
 
 
-class EmployeeApiView(generics.GenericAPIView):
-    permission_classes = [IsAuthenticated]
+class EmployeeApiView(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
+    lookup_field = "id"
 
     def get(self, request, *args, **kwargs):
-        if "id" in kwargs:
-            employee = self.get_object()
-            serializer = self.get_serializer(employee)
+        if "id" in self.kwargs:
+            return self.retrieve(request, *args, **kwargs)
 
-            return Response(
-                serializer.data,
-                status=status.HTTP_200_OK
-            )
-
-        employees = self.get_queryset()
-        serializer = self.get_serializer(
-            employees,
-            many=True
-        )
-
-        return Response(
-            serializer.data,
-            status=status.HTTP_200_OK
-        )
-
-    def get_object(self):
-        return generics.get_object_or_404(
-            self.get_queryset(),
-            id=self.kwargs["id"]
-        )
-
+        return self.list(request, *args, **kwargs)
