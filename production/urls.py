95c9c7c38049a9ab487da32b6b406528de69e066
usman <muhammadusman67200@gmail.com>
2026-08-18 13:07:21 +0500

    add url

--- production/urls.py
+++ production/urls.py
@@ -6,4 +6,6 @@ from .views import ProductionBatchApiView
 urlpatterns = [
     path("batches/",ProductionBatchApiView.as_view(),name="production-batch-list-create"),
     path("batches/<int:id>/",ProductionBatchApiView.as_view(),name="production-batch-detail"),
+    path("worker-assignments/",WorkerAssignmentApiView.as_view(),name="worker-assignment-list-create"),
+    path("worker-assignments/<int:id>/",WorkerAssignmentApiView.as_view(),name="worker-assignment-detail"),
 ]
